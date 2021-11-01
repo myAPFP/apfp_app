ec20b874e515b54f88aaf5725864557cd5af9e1a
Brian Walker <bkwalker@bsu.edu>
2021-11-01 16:15:47 -0400

    changes app entry point to welcome page

--- lib/welcome/welcome_widget.dart
+++ lib/welcome/welcome_widget.dart
@@ -5,7 +5,30 @@ import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
 import '../log_in_page/log_in_page_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'APFP',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en', '')],
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: WelcomeWidget(),
+    );
+  }
+}
 
 class WelcomeWidget extends StatefulWidget {
   WelcomeWidget({Key key}) : super(key: key);
