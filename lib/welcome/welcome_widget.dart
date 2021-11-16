6b238304fec933b372c4d0ae1e477268b6052134
Hailey Leonard <leonardhailey@gmail.com>
2021-11-15 19:35:38 -0500

    Refactors Welcome screen.

--- lib/welcome/welcome_widget.dart
+++ lib/welcome/welcome_widget.dart
@@ -1,34 +1,9 @@
 import '../create_account/create_account_widget.dart';
 import '../flutter_flow/flutter_flow_animations.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
 import '../log_in_page/log_in_page_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'APFP',
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [Locale('en', '')],
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: WelcomeWidget(),
-    );
-  }
-}
 
 class WelcomeWidget extends StatefulWidget {
   WelcomeWidget({Key key}) : super(key: key);
@@ -39,9 +14,6 @@ class WelcomeWidget extends StatefulWidget {
 
 class _WelcomeWidgetState extends State<WelcomeWidget>
     with TickerProviderStateMixin {
-  bool _loadingButton1 = false;
-  bool _loadingButton2 = false;
-  final scaffoldKey = GlobalKey<ScaffoldState>();
   final animationsMap = {
     'imageOnPageLoadAnimation': AnimationInfo(
       curve: Curves.easeIn,
@@ -57,6 +29,7 @@ class _WelcomeWidgetState extends State<WelcomeWidget>
       fadeIn: true,
     ),
   };
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
@@ -74,94 +47,62 @@ class _WelcomeWidgetState extends State<WelcomeWidget>
       key: scaffoldKey,
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/BSU_APFP_logo.png',
-                    width: 250,
-                    height: 200,
-                    fit: BoxFit.fitWidth,
-                  ).animated([animationsMap['imageOnPageLoadAnimation']])
-                ],
-              ),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome!',
-                  style: FlutterFlowTheme.bodyText1.override(
-                    fontFamily: 'Open Sans',
-                    color: FlutterFlowTheme.primaryColor,
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ).animated([animationsMap['textOnPageLoadAnimation']])
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
-                  child: FFButtonWidget(
-                    onPressed: () async {
-                      setState(() => _loadingButton1 = true);
-                      try {
-                        await Navigator.push(
-                          context,
-                          PageTransition(
-                            type: PageTransitionType.rightToLeft,
-                            duration: Duration(milliseconds: 125),
-                            reverseDuration: Duration(milliseconds: 125),
-                            child: LogInPageWidget(),
-                          ),
-                        );
-                      } finally {
-                        setState(() => _loadingButton1 = false);
-                      }
-                    },
-                    text: 'Log In',
-                    options: FFButtonOptions(
-                      width: 150,
-                      height: 50,
-                      color: Color(0xFFBA0C2F),
-                      textStyle: FlutterFlowTheme.subtitle2.override(
-                        fontFamily: 'Open Sans',
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      elevation: 2,
-                      borderSide: BorderSide(
-                        color: Colors.transparent,
-                        width: 1,
-                      ),
-                      borderRadius: 12,
-                    ),
-                    loading: _loadingButton1,
-                  ),
-                )
-              ],
-            ),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 60, 0, 0),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                decoration: BoxDecoration(
-                  color: Colors.white,
+        child: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Image.asset(
+                'assets/images/BSU_APFP_logo.png',
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 200,
+                fit: BoxFit.fitWidth,
+              ).animated([animationsMap['imageOnPageLoadAnimation']]),
+              Text(
+                'Welcome!',
+                style: FlutterFlowTheme.bodyText1.override(
+                  fontFamily: 'Open Sans',
+                  color: FlutterFlowTheme.primaryColor,
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
                 ),
-                child: Align(
-                  alignment: AlignmentDirectional(0, 0),
+              ).animated([animationsMap['textOnPageLoadAnimation']]),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 20),
+                child: FFButtonWidget(
+                  onPressed: () async {
+                    await Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LogInPageWidget(),
+                      ),
+                      (r) => false,
+                    );
+                  },
+                  text: 'Log In',
+                  options: FFButtonOptions(
+                    width: 150,
+                    height: 50,
+                    color: Color(0xFFBA0C2F),
+                    textStyle: FlutterFlowTheme.subtitle2.override(
+                      fontFamily: 'Open Sans',
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    elevation: 2,
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1,
+                    ),
+                    borderRadius: 12,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(10, 0, 10, 0),
                   child: Text(
                     'This app is intended for members of the Adult Physical Fitness Program at Ball State University. If you do not have an account, please contact an administrator at <EMAIL>.',
                     textAlign: TextAlign.center,
@@ -173,48 +114,40 @@ class _WelcomeWidgetState extends State<WelcomeWidget>
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-              child: FFButtonWidget(
-                onPressed: () async {
-                  setState(() => _loadingButton2 = true);
-                  try {
-                    await Navigator.push(
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                child: FFButtonWidget(
+                  onPressed: () async {
+                    await Navigator.pushAndRemoveUntil(
                       context,
-                      PageTransition(
-                        type: PageTransitionType.rightToLeft,
-                        duration: Duration(milliseconds: 125),
-                        reverseDuration: Duration(milliseconds: 125),
-                        child: CreateAccountWidget(),
+                      MaterialPageRoute(
+                        builder: (context) => CreateAccountWidget(),
                       ),
+                      (r) => false,
                     );
-                  } finally {
-                    setState(() => _loadingButton2 = false);
-                  }
-                },
-                text: 'Create Account',
-                options: FFButtonOptions(
-                  width: 200,
-                  height: 50,
-                  color: Colors.white,
-                  textStyle: FlutterFlowTheme.subtitle2.override(
-                    fontFamily: 'Open Sans',
-                    color: FlutterFlowTheme.primaryColor,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
+                  },
+                  text: 'Create Account',
+                  options: FFButtonOptions(
+                    width: 200,
+                    height: 50,
+                    color: Colors.white,
+                    textStyle: FlutterFlowTheme.subtitle2.override(
+                      fontFamily: 'Open Sans',
+                      color: FlutterFlowTheme.primaryColor,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    elevation: 2,
+                    borderSide: BorderSide(
+                      color: FlutterFlowTheme.secondaryColor,
+                      width: 3,
+                    ),
+                    borderRadius: 12,
                   ),
-                  elevation: 2,
-                  borderSide: BorderSide(
-                    color: FlutterFlowTheme.secondaryColor,
-                    width: 3,
-                  ),
-                  borderRadius: 12,
                 ),
-                loading: _loadingButton2,
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
