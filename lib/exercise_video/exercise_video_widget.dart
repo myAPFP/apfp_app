c2e483a674fb3c96db278f29e426cf0e04c78afd
Brian Walker <bkwalker@bsu.edu>
2021-11-15 22:41:46 -0500

    refactors code

--- lib/exercise_video/exercise_video_widget.dart
+++ lib/exercise_video/exercise_video_widget.dart
@@ -15,6 +15,8 @@ class ExerciseVideoWidget extends StatefulWidget {
 
 class _ExerciseVideoWidgetState extends State<ExerciseVideoWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  Text videoTitle, videoSource, exerciseType;
+  Text difficulty, descriptionHeader, descriptionBody;
 
   InkWell _goBackToAllVideos() {
     return InkWell(
@@ -36,6 +38,81 @@ class _ExerciseVideoWidgetState extends State<ExerciseVideoWidget> {
             )));
   }
 
+  void _setVideoTitle(String vTitle) {
+    setState(() {
+      videoTitle = Text(vTitle,
+          style: _bodyText1Style(fontSize: 30, fontWeight: FontWeight.bold));
+    });
+  }
+
+  void _setVideoSource(String vSource) {
+    setState(() {
+      videoSource = Text(vSource,
+          style: _bodyText1Style(fontSize: 16, fontWeight: FontWeight.normal));
+    });
+  }
+
+  void _setExerciseType(String eType) {
+    setState(() {
+      exerciseType = Text(eType,
+          style: _bodyText1Style(fontSize: 16, fontWeight: FontWeight.normal));
+    });
+  }
+
+  void _setDifficulty(String diff) {
+    setState(() {
+      difficulty = Text(diff,
+          style: _bodyText1Style(fontSize: 16, fontWeight: FontWeight.normal));
+    });
+  }
+
+  void _setDiscriptionHeader(String dHeader) {
+    setState(() {
+      descriptionHeader = Text(dHeader,
+          style: _bodyText1Style(fontSize: 20, fontWeight: FontWeight.bold));
+    });
+  }
+
+  void _setDiscriptionBody(String dBody) {
+    setState(() {
+      descriptionBody = Text(dBody,
+          style: _bodyText1Style(fontSize: 16, fontWeight: FontWeight.normal));
+    });
+  }
+
+  TextStyle _bodyText1Style({double fontSize, FontWeight fontWeight}) {
+    return FlutterFlowTheme.bodyText1.override(
+      fontFamily: 'Open Sans',
+      color: FlutterFlowTheme.primaryColor,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+    );
+  }
+
+  FlutterFlowYoutubePlayer _youtubePlayer() {
+    return FlutterFlowYoutubePlayer(
+      url: widget.url,
+      autoPlay: false,
+      looping: true,
+      mute: false,
+      showControls: true,
+      showFullScreen: true,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setVideoTitle('Example Video Title');
+    _setVideoSource("Video Source");
+    _setExerciseType("Type of Exercise");
+    _setDifficulty("Difficulty: example");
+    _setDiscriptionHeader("Description");
+    _setDiscriptionBody('Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        'Nullam cursus eu arcu vitae mattis' +
+        'Nullam sed gravida odio, nec ullamcorper justo. Ut ut finibus mauris.');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,82 +129,31 @@ class _ExerciseVideoWidgetState extends State<ExerciseVideoWidget> {
             ),
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(10, 25, 10, 0),
-              child: FlutterFlowYoutubePlayer(
-                url: widget.url,
-                autoPlay: false,
-                looping: true,
-                mute: false,
-                showControls: true,
-                showFullScreen: true,
-              ),
+              child: _youtubePlayer(),
             ),
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(15, 25, 15, 0),
-              child: Text(
-                'Example Video Title',
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Open Sans',
-                  color: FlutterFlowTheme.primaryColor,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+              child: videoTitle,
             ),
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(15, 10, 15, 0),
-              child: Text(
-                'Video source',
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Open Sans',
-                  color: FlutterFlowTheme.primaryColor,
-                  fontSize: 16,
-                ),
-              ),
+              child: videoSource,
             ),
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 0),
-              child: Text(
-                'Type of exercise',
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Open Sans',
-                  color: FlutterFlowTheme.primaryColor,
-                  fontSize: 16,
-                ),
-              ),
+              child: exerciseType,
             ),
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 0),
-              child: Text(
-                'Difficulty: example',
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Open Sans',
-                  color: FlutterFlowTheme.primaryColor,
-                  fontSize: 16,
-                ),
-              ),
+              child: difficulty,
             ),
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(15, 10, 15, 0),
-              child: Text(
-                'Description',
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Open Sans',
-                  color: FlutterFlowTheme.primaryColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+              child: descriptionHeader,
             ),
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 0),
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus eu arcu vitae mattis. Nullam sed gravida odio, nec ullamcorper justo. Ut ut finibus mauris. ',
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Open Sans',
-                  color: FlutterFlowTheme.primaryColor,
-                  fontSize: 16,
-                ),
-              ),
+              child: descriptionBody,
             )
           ],
         ),
