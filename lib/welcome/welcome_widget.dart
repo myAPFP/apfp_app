8594e382ff3cc669f40ce8af37fb2064aea7e239
Brian Walker <bkwalker@bsu.edu>
2021-11-22 16:29:43 -0500

    refactors code

--- lib/welcome/welcome_widget.dart
+++ lib/welcome/welcome_widget.dart
@@ -19,7 +19,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
