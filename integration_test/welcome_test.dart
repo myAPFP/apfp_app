3a391d931609b0c3f4ee2ad62b72c97ace799e32
Brian Walker <bkwalker@bsu.edu>
2021-12-07 12:18:12 -0500

    creates welcome_test.dart for testing

---
+++ integration_test/welcome_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:apfp/welcome/welcome_widget.dart' as app;
+import 'package:flutter/material.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the login button, verify navigation', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      var loginButton = find.byKey(Key('Welcome.LoginButton'));
+
+      // Expect to find the item on screen.
+      expect(loginButton, findsOneWidget);
+
+      // Emulates a tap on the login button.
+      await tester.tap(loginButton);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify naviagtion to login route after button press.
+      expect(find.text('Forgot Your Password?'), findsOneWidget);
+    });
+
+  testWidgets('tap on the create account button, verify navigation', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      var createAcctButton = find.byKey(Key('Welcome.CreateAcctButton'));
+
+      // Expect to find the item on screen.
+      expect(createAcctButton, findsOneWidget);
+
+      // Emulates a tap on the create account button.
+      await tester.tap(createAcctButton);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify naviagtion to create account route after button press.
+      expect(find.text('Welcome to the Adult Physical Fitness Program at Ball State University!' +
+                  ' Please enter the details below to create your account.'), findsOneWidget);
+    });
+  });
+}
