44e91adc0270cb30f75984f784bdc3c9a377119a
Brian Walker <bkwalker@bsu.edu>
2021-12-07 14:52:33 -0500

    adds no-input integration test

--- integration_test/create_acct_test.dart
+++ integration_test/create_acct_test.dart
@@ -1,30 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:apfp/welcome/welcome_widget.dart' as app;
+import 'package:flutter/material.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter',
+    testWidgets('Tap on the Create Account button with no input, verify each textfield displays an warning',
         (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
 
-      // Verify the counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-
-      // Finds the floating action button to tap on.
-      final Finder fab = find.byTooltip('Increment');
-
-      // Emulate a tap on the floating action button.
-      await tester.tap(fab);
-
-      // Trigger a frame.
+      // Launches Create Account Screen from Welcome
+      var createAcctButton_welcome = find.byKey(Key('Welcome.CreateAcctButton'));
+      await tester.tap(createAcctButton_welcome);
       await tester.pumpAndSettle();
 
-      // Verify the counter increments by 1.
-      expect(find.text('1'), findsOneWidget);
+      // Taps Create Account Button without any input
+      var createAcctButton = find.byKey(Key('Create.CreateAcctButton'));
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      // Verify each textfield displays a no-input warning.
+      expect(find.text('Please provide a value'), findsNWidgets(5));
     });
   });
 }
