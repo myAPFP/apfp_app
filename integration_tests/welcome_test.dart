c6b6f985dc105601e310708d1e8d8c3d91025598
Brian Walker <bkwalker@bsu.edu>
2021-12-08 15:58:40 -0500

    refactors import statements

--- integration_tests/welcome_test.dart
+++ integration_tests/welcome_test.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:apfp/welcome/welcome_widget.dart' as app;
-import 'package:flutter/material.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
