a136505c0523bc9d1920eb5b93f2526ba35e6643
Brian Walker <bkwalker@bsu.edu>
2021-12-12 17:17:14 -0500

    adds more testing to create acct

--- integration_tests/create_acct_test.dart
+++ integration_tests/create_acct_test.dart
@@ -28,6 +28,169 @@ void main() {
       expect(find.text('Please provide a value'), findsNWidgets(5));
     });
 
+    testWidgets("Enter invalid First Name, receieve a warning.",
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Launches Create Account Screen from Welcome
+      var createAcctButton_wel = find.byKey(Key('Welcome.createAcctButton'));
+      await tester.tap(createAcctButton_wel);
+      await tester.pumpAndSettle();
+
+      // Populates First Name textFormField with invalid info
+      await tester.enterText(
+          find.byKey(Key("Create.firstNameTextField")), '7n');
+      await tester.pumpAndSettle();
+
+      // Presses "Enter" - hides keyboard 
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle();
+
+      // Taps Create Account button, submitting the info entered
+      var createAcctButton = find.byKey(Key('Create.createAcctButton'));
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      // Verify First Name textFormField provides warning
+      expect(find.text('Please provide a valid first name'), findsNWidgets(1));
+    });
+
+    testWidgets("Enter invalid Last Name, receieve a warning.",
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Launches Create Account Screen from Welcome
+      var createAcctButton_wel = find.byKey(Key('Welcome.createAcctButton'));
+      await tester.tap(createAcctButton_wel);
+      await tester.pumpAndSettle();
+
+      // Populates Last Name textFormField with invalid info
+      await tester.enterText(find.byKey(Key("Create.lastNameTextField")), '7n');
+      await tester.pumpAndSettle();
+
+      // Presses "Enter" - hides keyboard 
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle();
+
+      // Taps Create Account button, submitting the info entered
+      var createAcctButton = find.byKey(Key('Create.createAcctButton'));
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      // Verify Last Name textFormField provides warning
+      expect(find.text('Please provide a valid last name'), findsNWidgets(1));
+    });
+
+    testWidgets("Enter invalid Email, receieve a warning.",
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Launches Create Account Screen from Welcome
+      var createAcctButton_wel = find.byKey(Key('Welcome.createAcctButton'));
+      await tester.tap(createAcctButton_wel);
+      await tester.pumpAndSettle();
+
+      // Populates Email textFormField with invalid info
+      await tester.enterText(
+          find.byKey(Key("Create.emailTextField")), 'example');
+      await tester.pumpAndSettle();
+
+      // Presses "Enter" - hides keyboard 
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle();
+
+      // Taps Create Account button, submitting the info entered
+      var createAcctButton = find.byKey(Key('Create.createAcctButton'));
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      // Verify Email textFormField provides warning
+      expect(
+          find.text('Please provide a valid email address'), findsNWidgets(1));
+    });
+
+    testWidgets("Enter invalid password, receieve a warning.",
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Launches Create Account Screen from Welcome
+      var createAcctButton_wel = find.byKey(Key('Welcome.createAcctButton'));
+      await tester.tap(createAcctButton_wel);
+      await tester.pumpAndSettle();
+
+      // Populates Password textFormField with invalid info
+      await tester.enterText(
+          find.byKey(Key("Create.passwordTextField")), 'pass');
+      await tester.pumpAndSettle();
+
+      // Presses "Enter" - hides keyboard 
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle();
+
+      // Taps Create Account button, submitting the info entered
+      var createAcctButton = find.byKey(Key('Create.createAcctButton'));
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      // Verify password textFormField provides warning
+      expect(
+          find.text('Please provide a valid password'), findsNWidgets(1));
+    });
+
+    testWidgets(
+        "Enter non-matching confirmed password, receieve a warning.",
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Launches Create Account Screen from Welcome
+      var createAcctButton_wel = find.byKey(Key('Welcome.createAcctButton'));
+      await tester.tap(createAcctButton_wel);
+      await tester.pumpAndSettle();
+
+      // Populates Password textFormField with valid info
+      await tester.enterText(
+          find.byKey(Key('Create.passwordTextField')), 'password12!');
+      await tester.pumpAndSettle();
+
+      // Populates ConfirmPassword textFormField with non-matching info
+      await tester.enterText(
+          find.byKey(Key("Create.confirmPasswordTextField")), 'pass');
+      await tester.pumpAndSettle();
+
+      // Presses "Enter" - hides keyboard 
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle();
+
+      // Taps Create Account button, submitting the info entered
+      var createAcctButton = find.byKey(Key('Create.createAcctButton'));
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      await tester.tap(createAcctButton);
+      await tester.pumpAndSettle();
+
+      // Verify ConfirmPassword textFormField provides warning
+      expect(
+          find.text('Passwords must match'), findsNWidgets(1));
+    });
+
     testWidgets(
         "US: I can enter my name, email address, and passwords through the" +
             "use of text fields.", (WidgetTester tester) async {
@@ -39,7 +202,7 @@ void main() {
       await tester.tap(createAcctButton_wel);
       await tester.pumpAndSettle();
 
-      // Populates each textformfield with valid info
+      // Populates each textFormField with valid info
       await tester.enterText(
           find.byKey(Key("Create.firstNameTextField")), 'John');
       await tester.pumpAndSettle();
@@ -71,7 +234,7 @@ void main() {
       await tester.tap(createAcctButton_wel);
       await tester.pumpAndSettle();
 
-      // Populates each textformfield with valid info
+      // Populates each textFormField with valid info
       await tester.enterText(
           find.byKey(Key("Create.firstNameTextField")), 'John');
       await tester.pumpAndSettle();
@@ -109,7 +272,7 @@ void main() {
       await tester.tap(createAcctButton_wel);
       await tester.pumpAndSettle();
 
-      // Populates PW textformfields with example password
+      // Populates PW textFormFields with example password
       await tester.enterText(
           find.byKey(Key('Create.passwordTextField')), 'password12!');
       await tester.pumpAndSettle();
@@ -117,7 +280,7 @@ void main() {
           find.byKey(Key('Create.confirmPasswordTextField')), 'password12!');
       await tester.pumpAndSettle();
 
-      // Toggles each password textfieldform visibility
+      // Toggles each password textFormField visibility
       await tester.tap(find.byKey(Key("Create.pWVisibilty")));
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(Key("Create.confirmPWVisibilty")));
@@ -126,11 +289,17 @@ void main() {
       // Verify the entered password is shown
       expect(find.text('password12!'), findsNWidgets(2));
 
-      // Toggles each password textfieldform visibility again
+      // Verify the visibility icons changes after tap
+      expect(find.byIcon(Icons.visibility_outlined), findsNWidgets(2));
+
+      // Toggles each password textFormField visibility again
       await tester.tap(find.byKey(Key("Create.pWVisibilty")));
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(Key("Create.confirmPWVisibilty")));
       await tester.pumpAndSettle();
+
+      // Verify the visibility icons changes after tap
+      expect(find.byIcon(Icons.visibility_off_outlined), findsNWidgets(2));
     });
   });
 }
