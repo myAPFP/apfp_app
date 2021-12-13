7d8d06d32ff00a4faa38f668dc36bbc82c4ec8f4
Caleb Van Lue <cpvanlue@bsu.edu>
2021-12-13 13:57:03 -0500

    Add integration testing for Home page

---
+++ integration_tests/home_test.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:apfp/welcome/welcome_widget.dart' as app;
+
+// These tests run from the Home page; please reach this page first on your system to
+// run these tests successfully
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Home Screen Tests', () {
+    testWidgets(
+        'US: I am able to view the few most recent announcements in an area at the top of the screen. ' +
+            'I am displayed their title', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Ensure announcements panel exists
+      expect(find.text('Recent\nAnnouncements'), findsOneWidget);
+      expect(find.byKey(Key('Home.announcements')), findsOneWidget);
+
+      // Ensure three announcement titles are shown
+      expect(find.textContaining('Alert '), findsNWidgets(3));
+    });
+
+// The following test is quite rudimentary; ensures existence of the Today's Activity panel.
+// This feature has yet to be implemented.
+
+    testWidgets(
+        'US: My activity that has been logged for the day is shown in a graphical representation. ' +
+            'If I tap on it, I am taken to the My Activity page.',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Ensure Today's Activity header and activity GUI (placeholder) exist
+      expect(find.text("Today's Activity"), findsOneWidget);
+      expect(find.byKey(Key('Home.activityGUI')), findsOneWidget);
+    });
+  });
+}
