66c29fce0eb51b46d95bbe6b9d6c63b2da28673e
Brian Walker <bkwalker@bsu.edu>
2021-12-14 19:51:24 -0500

    completes test

--- integration_tests/add_activity_test.dart
+++ integration_tests/add_activity_test.dart
@@ -18,19 +18,18 @@ import 'package:apfp/welcome/welcome_widget.dart' as app;
   ?  );
 
   ! This is done to prevent animation tickers from stalling our tests, causing 
-  ! them to all fail.
+  ! them to all fail. Before launching the app in Debugging mode again, 
+  ! you MUST un-comment out the above animation code.
 */
-
-//
-//  I can submit the information to be stored in the application.
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('My Activity Integration Tests', () {
     testWidgets(
-        'US:I can input the name of the activity, the type of activity, and duration' +
-            'of the activity through a combination of text boxes and dropdown menus.',
+        'US: I can input the name of the activity, the type of activity,' +
+            'calories burned and duration of the activity via text boxes.' +
+        'US: I can submit the information to be stored in the application.',
         (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
@@ -43,37 +42,33 @@ void main() {
       await tester.tap(find.byKey(Key("Activity.addActivityButtton")));
       await tester.pumpAndSettle();
 
-      
-
-      // Adds blank new activity to enable page scrolling
-      await tester.tap(find.byKey(Key("AddActivity.submitButton")));
+      // Populates each textformfield with info
+      await tester.enterText(
+          find.byKey(Key("AddActivity.activityNameTextField")), 'Jogging');
       await tester.pumpAndSettle();
-    });
-
-    testWidgets(
-        "US: If I am ready to log another activity, I can press a"
-                "button to be taken to a screen to log a new activity" +
-            "US: The items are stored in a list so that if I've logged a lot of" +
-            "activity, I can scroll through it.", (WidgetTester tester) async {
-      app.main();
+      await tester.enterText(
+          find.byKey(Key("AddActivity.totalCalTextField")), '100');
       await tester.pumpAndSettle();
-
-      // Reach My Activity Page to verify its existence
-      await tester.tap(find.byTooltip('My Activity'));
+      await tester.enterText(
+          find.byKey(Key("AddActivity.exerciseTextField")), 'Cardio');
+      await tester.pumpAndSettle();
+      await tester.enterText(
+          find.byKey(Key("AddActivity.durationTextField")), '10 min');
       await tester.pumpAndSettle();
 
-      // Navigates to new activity screen
-      await tester.tap(find.byKey(Key("Activity.addActivityButtton")));
+      // Presses "Enter" - hides keyboard 
+      await tester.testTextInput.receiveAction(TextInputAction.done);
       await tester.pumpAndSettle();
 
-      // Adds blank new activity to enable page scrolling
+      // Adds new activity 
       await tester.tap(find.byKey(Key("AddActivity.submitButton")));
       await tester.pumpAndSettle();
 
-      // Verifies page can scroll
-      await tester.drag(find.byKey(Key("Activity.singleChildScrollView")),
-          const Offset(0.0, -300));
-      await tester.pump();
+      // Verifies new info is stored
+      expect(find.byIcon(Icons.info), findsOneWidget);
+      expect(find.text("10 min                       100 cals"), findsOneWidget);
+      expect(find.text("Jogging"), findsOneWidget);
+      expect(find.text("Cardio"), findsNWidgets(4));
     });
   });
 }
