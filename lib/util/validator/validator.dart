5d9d6169539a63c88ed91887d86efced5b356977
Brian Walker <bkwalker@bsu.edu>
2022-01-19 20:22:23 -0500

    makes methods private

--- lib/util/validator/validator.dart
+++ lib/util/validator/validator.dart
@@ -1,14 +1,14 @@
 class Validator {
 
   // Matches most names, including those that contains spaces
-  static RegExp validNameRegex =
+  static RegExp _validNameRegex =
       new RegExp(r"^[\w'\-][^,.0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$");
 
   // Matches numbers in XXX-XXX-XXXX format
-  static RegExp validPhoneRegex = new RegExp(r"\d{3}-\d{3}-\d{4}");
+  static RegExp _validPhoneRegex = new RegExp(r"\d{3}-\d{3}-\d{4}");
 
   // Matches valid email addresses
-  static RegExp validEmailRegex = new RegExp(
+  static RegExp _validEmailRegex = new RegExp(
       r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}" +
           r"[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
 
@@ -23,16 +23,16 @@ class Validator {
       r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");
 
   static bool isValidName(String name) {
-    return validNameRegex.hasMatch(name);
+    return _validNameRegex.hasMatch(name);
   }
 
   static bool isValidPhone(String phone) {
     if (phone.length != 12) return false;
-    return validPhoneRegex.hasMatch(phone);
+    return _validPhoneRegex.hasMatch(phone);
   }
 
   static bool isValidEmail(String email) {
-    return validEmailRegex.hasMatch(email);
+    return _validEmailRegex.hasMatch(email);
   }
 
   static bool isValidPassword(String password) {
