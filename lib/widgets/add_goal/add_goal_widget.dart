b19258de4986f3b16aed666ec400dacfeccd7fce
Brian Walker <bkwalker@bsu.edu>
2022-02-24 16:30:26 -0500

    refactors code to reduce redudancy

--- lib/widgets/add_goal/add_goal_widget.dart
+++ lib/widgets/add_goal/add_goal_widget.dart
@@ -1,6 +1,8 @@
 import 'package:apfp/firebase/firestore.dart';
+import 'package:apfp/flutter_flow/flutter_flow_util.dart';
 import 'package:apfp/util/toasted/toasted.dart';
 import 'package:apfp/util/validator/validator.dart';
+import 'package:apfp/widgets/confimation_dialog/confirmation_dialog.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import 'package:flutter/material.dart';
@@ -23,7 +25,7 @@ class AddGoalWidget extends StatefulWidget {
 
 class _AddGoalWidgetState extends State<AddGoalWidget> {
   bool _loadingButton = false;
-  final _formKey = GlobalKey<FormState>();
+  final _exerciseFormKey = GlobalKey<FormState>();
   final scaffoldKey = GlobalKey<ScaffoldState>();
   TextEditingController? exerciseGoalController = TextEditingController();
 
@@ -61,21 +63,47 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
     return double.parse(exerciseGoalController!.text.toString());
   }
 
-  FFButtonWidget _setExerciseGoalButton() {
+  InkWell _goBackButton() {
+    return InkWell(
+        onTap: () => Navigator.pop(context),
+        child: Text('< Go Back', style: FlutterFlowTheme.subtitle2));
+  }
+
+  Expanded _deleteGoalIcon({required Function onDelete}) {
+    return Expanded(
+        child: InkWell(
+            onTap: () {
+              ConfirmationDialog.showConfirmationDialog(
+                  context: context,
+                  title: Text('Reset Goal'),
+                  content: Text('Are you sure you want to reset this goal?'),
+                  onSubmitTap: () {
+                    onDelete();
+                    Navigator.of(context).pop();
+                    showSnackbar(context, "Goal has been reset");
+                  },
+                  onCancelTap: () => Navigator.of(context).pop(),
+                  submitText: "Reset",
+                  cancelText: "Back");
+            },
+            child: Icon(
+              Icons.delete,
+              size: 45,
+              color: FlutterFlowTheme.secondaryColor,
+            )));
+  }
+
+  FFButtonWidget _setGoalButton(GlobalKey<FormState> formKey, Function onTap) {
     return FFButtonWidget(
       onPressed: () async {
-        if (_formKey.currentState!.validate()) {
+        if (formKey.currentState!.validate()) {
           FocusScope.of(context).unfocus();
           setState(() {
             _loadingButton = true;
           });
-          await FireStore.updateHealthData(
-                  FireStore.exerciseTimeEndGoalToMap(_getExerciseEndGoal()))
-              .then((value) {
-            FireStore.updateHealthData(FireStore.exerciseGoalBoolToMap(true));
-            Toasted.showToast("Goal has been set");
-            setState(() => _loadingButton = false);
-          });
+          onTap();
+          showSnackbar(context, "Goal has been set");
+          setState(() => _loadingButton = false);
         }
       },
       text: 'Set Goal',
@@ -99,12 +127,6 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
     );
   }
 
-  InkWell _goBackButton() {
-    return InkWell(
-        onTap: () => Navigator.pop(context),
-        child: Text('< Go Back', style: FlutterFlowTheme.subtitle2));
-  }
-
   Padding _exerciseGoalTextField() {
     return Padding(
       padding: EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
@@ -112,8 +134,11 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
         width: MediaQuery.of(context).size.width / 2.5,
         child: TextFormField(
           validator: (value) {
-            if (!Validator.isInt(value!)) {
-              return "Please use whole numbers (1+) only";
+            if (value == null || value.isEmpty) {
+              return "Please provide a value";
+            }
+            if (!Validator.isInt(value)) {
+              return "Integers (1+) only";
             }
             if (double.parse(value) < 10) {
               return "Goal must be at least 10 min";
@@ -158,52 +183,50 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
           backgroundColor: Colors.white,
           body: SafeArea(
               child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(15, 20, 0, 20),
-                      child: _goBackButton(),
-                    ),
-                    Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(15, 20, 0, 0),
-                        child: _header(
-                            text: 'Daily Goals',
-                            style: FlutterFlowTheme.title1)),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(15, 30, 0, 5),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(15, 20, 0, 20),
+                    child: _goBackButton(),
+                  ),
+                  Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(15, 20, 0, 0),
                       child: _header(
-                          text: 'Exercise Goal',
-                          style: FlutterFlowTheme.title3),
-                    ),
-                    Row(
+                          text: 'Daily Goals', style: FlutterFlowTheme.title1)),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(15, 30, 0, 5),
+                    child: _header(
+                        text: 'Exercise Goal', style: FlutterFlowTheme.title3),
+                  ),
+                  Form(
+                    key: _exerciseFormKey,
+                    child: Row(
                       children: [
                         _exerciseGoalTextField(),
-                        _setExerciseGoalButton(),
-                        Expanded(
-                            child: InkWell(
-                                onTap: () async {
-                                  await FireStore.updateHealthData(
-                                          FireStore.exerciseTimeEndGoalToMap(0.0))
-                                      .then((value) {
-                                    FireStore.updateHealthData(
-                                        FireStore.exerciseGoalBoolToMap(false));
-                                    exerciseGoalController!.text = '';
-                                    Toasted.showToast("Goal has been reset");
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.delete,
-                                  size: 45,
-                                  color: FlutterFlowTheme.secondaryColor,
-                                )))
+                        _setGoalButton(_exerciseFormKey, () async {
+                          await FireStore.updateHealthData(
+                                  FireStore.exerciseTimeEndGoalToMap(
+                                      _getExerciseEndGoal()))
+                              .then((value) {
+                            FireStore.updateHealthData(
+                                FireStore.exerciseGoalBoolToMap(true));
+                          });
+                        }),
+                        _deleteGoalIcon(onDelete: () async {
+                          await FireStore.updateHealthData(
+                                  FireStore.exerciseTimeEndGoalToMap(0.0))
+                              .then((value) {
+                            FireStore.updateHealthData(
+                                FireStore.exerciseGoalBoolToMap(false));
+                            exerciseGoalController!.text = '';
+                          });
+                        })
                       ],
                     ),
-                  ]),
-            ),
+                  ),
+                ]),
           ))),
     );
   }
