53431150d67c1a1a6e8c93a0d0b1adcae4171e24
Brian Walker <bkwalker@bsu.edu>
2022-02-26 20:05:06 -0500

    deletes unused code

--- lib/firebase/firestore.dart
+++ lib/firebase/firestore.dart
@@ -189,16 +189,5 @@ class FireStore {
       "exerciseTimeEndGoal": minutes,
     };
   }
-
-  static Map<String, dynamic> customGoalBoolToMap(String customGoalField, bool isGoalSet) {
-    return {
-      customGoalField: isGoalSet,
-    };
-  }
-
-  static Map<String, dynamic> customGoalToMap(String customGoalField, double isGoalSet) {
-    return {
-      customGoalField: isGoalSet,
-    };
-  }
+  
 }
