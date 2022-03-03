6f523f22fab7f94fe01e1760fb67a2b30e262de9
Brian Walker <bkwalker@bsu.edu>
2022-03-02 22:10:57 -0500

    adds custom weekly goal text fields

--- lib/widgets/add_goal/add_goal_widget.dart
+++ lib/widgets/add_goal/add_goal_widget.dart
@@ -2,6 +2,7 @@ import 'package:apfp/firebase/firestore.dart';
 import 'package:apfp/flutter_flow/flutter_flow_util.dart';
 import 'package:apfp/util/validator/validator.dart';
 import 'package:apfp/widgets/confimation_dialog/confirmation_dialog.dart';
+import '../../util/toasted/toasted.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import 'package:flutter/material.dart';
@@ -28,14 +29,22 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
   final _exerciseFormKey = GlobalKey<FormState>();
   final _exerciseWeeklyFormKey = GlobalKey<FormState>();
   final _cyclingFormKey = GlobalKey<FormState>();
+  final _cyclingWeeklyFormKey = GlobalKey<FormState>();
   final _rowingFormKey = GlobalKey<FormState>();
+  final _rowingWeeklyFormKey = GlobalKey<FormState>();
   final _stepMillFormKey = GlobalKey<FormState>();
+  final _stepMillWeeklyFormKey = GlobalKey<FormState>();
   final scaffoldKey = GlobalKey<ScaffoldState>();
-  TextEditingController? exerciseGoalController = TextEditingController();
-  TextEditingController? exerciseWeeklyGoalController = TextEditingController();
-  TextEditingController? cyclingGoalController = TextEditingController();
-  TextEditingController? rowingGoalController = TextEditingController();
-  TextEditingController? stepMillGoalController = TextEditingController();
+  TextEditingController? _exerciseGoalController = TextEditingController();
+  TextEditingController? _exerciseWeeklyGoalController =
+      TextEditingController();
+  TextEditingController? _cyclingGoalController = TextEditingController();
+  TextEditingController? _cyclingWeeklyGoalController = TextEditingController();
+  TextEditingController? _rowingGoalController = TextEditingController();
+  TextEditingController? _rowingWeeklyGoalController = TextEditingController();
+  TextEditingController? _stepMillGoalController = TextEditingController();
+  TextEditingController? _stepMillWeeklyGoalController =
+      TextEditingController();
 
   @override
   void initState() {
@@ -46,20 +55,23 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
   @override
   void dispose() {
     super.dispose();
-    exerciseGoalController!.dispose();
-    exerciseWeeklyGoalController!.dispose();
-    cyclingGoalController!.dispose();
-    rowingGoalController!.dispose();
-    stepMillGoalController!.dispose();
+    _exerciseGoalController!.dispose();
+    _exerciseWeeklyGoalController!.dispose();
+    _cyclingGoalController!.dispose();
+    _rowingGoalController!.dispose();
+    _stepMillGoalController!.dispose();
+    _cyclingWeeklyGoalController!.dispose();
+    _rowingWeeklyGoalController!.dispose();
+    _stepMillWeeklyGoalController!.dispose();
   }
 
   void setText(MapEntry<String, dynamic> element, String fieldName,
-      TextEditingController? controller) {
+      TextEditingController? contr) {
     if (element.key == fieldName) {
       if (element.value == 0.0) {
-        controller!.text = '';
+        contr!.text = '';
       } else {
-        controller!.text = element.value.round().toString();
+        contr!.text = element.value.round().toString();
       }
     }
   }
@@ -68,11 +80,14 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
     var doc = await FireStore.getHealthDocument().get();
     var docEntries = doc.data()!.entries;
     docEntries.forEach((element) {
-      setText(element, 'exerciseTimeEndGoal', exerciseGoalController);
-      setText(element, 'cyclingEndGoal', cyclingGoalController);
-      setText(element, 'rowingEndGoal', rowingGoalController);
-      setText(element, 'stepMillEndGoal', stepMillGoalController);
-      setText(element, "exerciseTimeEndGoal_w", exerciseWeeklyGoalController);
+      setText(element, 'exerciseTimeEndGoal', _exerciseGoalController);
+      setText(element, 'cyclingEndGoal', _cyclingGoalController);
+      setText(element, 'rowingEndGoal', _rowingGoalController);
+      setText(element, 'stepMillEndGoal', _stepMillGoalController);
+      setText(element, "exerciseTimeEndGoal_w", _exerciseWeeklyGoalController);
+      setText(element, "cyclingEndGoal_w", _cyclingWeeklyGoalController);
+      setText(element, "rowingEndGoal_w", _rowingWeeklyGoalController);
+      setText(element, "stepMillEndGoal_w", _stepMillWeeklyGoalController);
     });
   }
 
@@ -95,7 +110,7 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
             _loadingButton = true;
           });
           onTap();
-          showSnackbar(context, "Goal has been set");
+          Toasted.showToast("Goal has been set");
           setState(() => _loadingButton = false);
         }
       },
@@ -116,7 +131,7 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
                   onSubmitTap: () {
                     onDelete();
                     Navigator.of(context).pop();
-                    showSnackbar(context, "Goal has been reset");
+                    Toasted.showToast("Goal has been reset");
                   },
                   onCancelTap: () => Navigator.of(context).pop(),
                   submitText: "Reset",
@@ -144,7 +159,8 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
     );
   }
 
-  Padding _goalTextField(String hintText, TextEditingController controller) {
+  Padding _goalTextField(
+      {required String hintText, required TextEditingController contr}) {
     return Padding(
       padding: EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
       child: Container(
@@ -165,7 +181,7 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
             }
             return null;
           },
-          controller: controller,
+          controller: contr,
           obscureText: false,
           decoration: InputDecoration(
             hintText: hintText,
@@ -201,11 +217,12 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
         key: _exerciseFormKey,
         child: Row(
           children: [
-            _goalTextField("Total Minutes", exerciseGoalController!),
+            _goalTextField(
+                hintText: "Total Minutes", contr: _exerciseGoalController!),
             _setGoalButton(_exerciseFormKey, () async {
               await FireStore.updateHealthData(
                       FireStore.exerciseTimeEndGoalToMap(double.parse(
-                          exerciseGoalController!.text.toString())))
+                          _exerciseGoalController!.text.toString())))
                   .then((value) {
                 FireStore.updateHealthData(
                     FireStore.exerciseGoalBoolToMap(true));
@@ -217,7 +234,7 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
                   .then((value) {
                 FireStore.updateHealthData(
                     FireStore.exerciseGoalBoolToMap(false));
-                exerciseGoalController!.text = '';
+                _exerciseGoalController!.text = '';
               });
             })
           ],
@@ -231,11 +248,12 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
         key: _cyclingFormKey,
         child: Row(
           children: [
-            _goalTextField("Total Minutes", cyclingGoalController!),
+            _goalTextField(
+                hintText: "Total Minutes", contr: _cyclingGoalController!),
             _setGoalButton(_cyclingFormKey, () async {
               await FireStore.updateHealthData({
                 "cyclingEndGoal":
-                    double.parse(cyclingGoalController!.text.toString())
+                    double.parse(_cyclingGoalController!.text.toString())
               }).then((value) {
                 FireStore.updateHealthData({"isCyclingGoalSet": true});
               });
@@ -244,7 +262,7 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
               await FireStore.updateHealthData({"cyclingEndGoal": 0})
                   .then((value) {
                 FireStore.updateHealthData({"isCyclingGoalSet": false});
-                cyclingGoalController!.text = '';
+                _cyclingGoalController!.text = '';
               });
             })
           ],
@@ -258,11 +276,12 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
         key: _rowingFormKey,
         child: Row(
           children: [
-            _goalTextField("Total Minutes", rowingGoalController!),
+            _goalTextField(
+                hintText: "Total Minutes", contr: _rowingGoalController!),
             _setGoalButton(_rowingFormKey, () async {
               await FireStore.updateHealthData({
                 "rowingEndGoal":
-                    double.parse(rowingGoalController!.text.toString())
+                    double.parse(_rowingGoalController!.text.toString())
               }).then((value) {
                 FireStore.updateHealthData({"isRowingGoalSet": true});
               });
@@ -271,7 +290,7 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
               await FireStore.updateHealthData({"rowingEndGoal": 0})
                   .then((value) {
                 FireStore.updateHealthData({"isRowingGoalSet": false});
-                rowingGoalController!.text = '';
+                _rowingGoalController!.text = '';
               });
             })
           ],
@@ -285,11 +304,12 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
         key: _stepMillFormKey,
         child: Row(
           children: [
-            _goalTextField("Total Minutes", stepMillGoalController!),
+            _goalTextField(
+                hintText: "Total Minutes", contr: _stepMillGoalController!),
             _setGoalButton(_stepMillFormKey, () async {
               await FireStore.updateHealthData({
                 "stepMillEndGoal":
-                    double.parse(stepMillGoalController!.text.toString())
+                    double.parse(_stepMillGoalController!.text.toString())
               }).then((value) {
                 FireStore.updateHealthData({"isStepMillGoalSet": true});
               });
@@ -298,7 +318,7 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
               await FireStore.updateHealthData({"stepMillEndGoal": 0})
                   .then((value) {
                 FireStore.updateHealthData({"isStepMillGoalSet": false});
-                stepMillGoalController!.text = '';
+                _stepMillGoalController!.text = '';
               });
             })
           ],
@@ -317,11 +337,13 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
         key: _exerciseWeeklyFormKey,
         child: Row(
           children: [
-            _goalTextField("Total Minutes", exerciseWeeklyGoalController!),
+            _goalTextField(
+                hintText: "Total Minutes",
+                contr: _exerciseWeeklyGoalController!),
             _setGoalButton(_exerciseWeeklyFormKey, () async {
               await FireStore.updateHealthData({
                 "exerciseTimeEndGoal_w":
-                    double.parse(exerciseWeeklyGoalController!.text.toString())
+                    double.parse(_exerciseWeeklyGoalController!.text.toString())
               }).then((value) {
                 FireStore.updateHealthData({"isExerciseTimeGoalSet_w": true});
               });
@@ -330,7 +352,94 @@ class _AddGoalWidgetState extends State<AddGoalWidget> {
               await FireStore.updateHealthData({"exerciseTimeEndGoal_w": 0})
                   .then((value) {
                 FireStore.updateHealthData({"isExerciseTimeGoalSet_w": false});
-                exerciseWeeklyGoalController!.text = '';
+                _exerciseWeeklyGoalController!.text = '';
+              });
+            })
+          ],
+        ),
+      ),
+      Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(15, 30, 0, 5),
+        child: _header(text: 'Cycling Goal', style: FlutterFlowTheme.title3),
+      ),
+      Form(
+        key: _cyclingWeeklyFormKey,
+        child: Row(
+          children: [
+            _goalTextField(
+                hintText: "Total Minutes",
+                contr: _cyclingWeeklyGoalController!),
+            _setGoalButton(_cyclingWeeklyFormKey, () async {
+              await FireStore.updateHealthData({
+                "cyclingEndGoal_w":
+                    double.parse(_cyclingWeeklyGoalController!.text.toString())
+              }).then((value) {
+                FireStore.updateHealthData({"isCyclingGoalSet_w": true});
+              });
+            }),
+            _deleteGoalIcon(onDelete: () async {
+              await FireStore.updateHealthData({"cyclingEndGoal_w": 0})
+                  .then((value) {
+                FireStore.updateHealthData({"isCyclingGoalSet_w": false});
+                _cyclingWeeklyGoalController!.text = '';
+              });
+            })
+          ],
+        ),
+      ),
+      Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(15, 30, 0, 5),
+        child: _header(text: 'Rowing Goal', style: FlutterFlowTheme.title3),
+      ),
+      Form(
+        key: _rowingWeeklyFormKey,
+        child: Row(
+          children: [
+            _goalTextField(
+                hintText: "Total Minutes", contr: _rowingWeeklyGoalController!),
+            _setGoalButton(_rowingWeeklyFormKey, () async {
+              await FireStore.updateHealthData({
+                "rowingEndGoal_w":
+                    double.parse(_rowingWeeklyGoalController!.text.toString())
+              }).then((value) {
+                FireStore.updateHealthData({"isRowingGoalSet_w": true});
+              });
+            }),
+            _deleteGoalIcon(onDelete: () async {
+              await FireStore.updateHealthData({"rowingEndGoal_w": 0})
+                  .then((value) {
+                FireStore.updateHealthData({"isRowingGoalSet_w": false});
+                _rowingWeeklyGoalController!.text = '';
+              });
+            })
+          ],
+        ),
+      ),
+
+
+      Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(15, 30, 0, 5),
+        child: _header(text: 'Step Mill Goal', style: FlutterFlowTheme.title3),
+      ),
+      Form(
+        key: _stepMillWeeklyFormKey,
+        child: Row(
+          children: [
+            _goalTextField(
+                hintText: "Total Minutes", contr: _stepMillWeeklyGoalController!),
+            _setGoalButton(_stepMillWeeklyFormKey, () async {
+              await FireStore.updateHealthData({
+                "stepMillEndGoal_w":
+                    double.parse(_stepMillWeeklyGoalController!.text.toString())
+              }).then((value) {
+                FireStore.updateHealthData({"isStepMillGoalSet_w": true});
+              });
+            }),
+            _deleteGoalIcon(onDelete: () async {
+              await FireStore.updateHealthData({"stepMillEndGoal_w": 0})
+                  .then((value) {
+                FireStore.updateHealthData({"isStepMillGoalSet_w": false});
+                _stepMillWeeklyGoalController!.text = '';
               });
             })
           ],
