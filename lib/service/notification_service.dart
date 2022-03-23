bd4a9c9aae2ce0e2a039dde9eef23fdb831f1241
Brian Walker <bkwalker@bsu.edu>
2022-03-23 18:33:54 -0400

    adds app icon to goal notification

--- lib/service/notification_service.dart
+++ lib/service/notification_service.dart
@@ -10,14 +10,13 @@ class NotificationService {
   static AndroidNotificationChannel _channel = AndroidNotificationChannel(
       'high_importance_channel', // id
       'High Importance Notifications', // title
-      description:
-          'This channel is used for important _notifications.',
+      description: 'This channel is used for important _notifications.',
       importance: Importance.high,
       playSound: true);
 
   static Future init() async {
     final iOS = IOSInitializationSettings();
-    final android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    final android = AndroidInitializationSettings('app_icon');
     final settings = InitializationSettings(android: android, iOS: iOS);
     await _notifications
         .resolvePlatformSpecificImplementation<
@@ -28,17 +27,21 @@ class NotificationService {
     });
   }
 
-  static void showGoalNotification(String title, String body, {int id = 0, String type = "Daily"}) {
+  static void showGoalNotification(String title, String body,
+      {int id = 0, String type = "Daily"}) {
     _notifications.show(
         id,
         title,
         body,
         NotificationDetails(
-            android: AndroidNotificationDetails(_channel.id, _channel.name,
-                channelDescription: _channel.description,
-                importance: Importance.high,
-                color: FlutterFlowTheme.secondaryColor,
-                playSound: true,
-                icon: '@mipmap/ic_launcher')), payload: type);
+            android: AndroidNotificationDetails(
+          _channel.id,
+          _channel.name,
+          channelDescription: _channel.description,
+          importance: Importance.high,
+          color: FlutterFlowTheme.secondaryColor,
+          playSound: true
+        )),
+        payload: type);
   }
 }
