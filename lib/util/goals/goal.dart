c52055d3494e2f181e1123de225d215478ae5152
Brian Walker Jr <thaballa79@gmail.com>
2022-04-11 00:00:46 -0400

    adds documentation

--- lib/util/goals/goal.dart
+++ lib/util/goals/goal.dart
@@ -1,106 +1,332 @@
-import 'package:apfp/service/notification_service.dart';
-import '../../firebase/firestore.dart';
+import '/firebase/firestore.dart';
+import '/service/notification_service.dart';
 
 class Goal {
+
+  /// Stores a [DateTime] instance with current date and time in the local time zone
   static final _now = DateTime.now();
 
+  /// Stores current day number.
+  ///
+  /// Example:
+  /// - Date: 3/23/2022
+  /// - dayOfMonth = 23
   static int dayOfMonth = 0;
 
+  /// Indicates if the user is displaying daily goals in Home.
+  ///
+  /// If this is false, weekly goals are being displayed.
   static bool isDailyGoalsDisplayed = false;
 
+  /// Indicates if the user has synchronized a health app to myAPFP
   static bool isHealthAppSynced = false;
 
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String exerciseWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String calWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String stepWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String mileWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String cyclingWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String rowingWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String stepMillWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String ellipticalWeekDeadline = "0/00/0000";
+
+  /// Serves as a goal deadline. When a user sets a weekly goal, 
+  /// this is updated to the week from the current date.
+  ///
+  /// When a user resets a weekly goal, this becomes its initial value.
+  ///
+  /// Example:
+  /// - "0/00/0000" -> "3/23/2022"
   static String resistanceStrengthWeekDeadline = "0/00/0000";
 
+  /// Stores a user's current daily goal progess for exercise time.
   static double userProgressExerciseTime = 0;
+
+  /// Stores a user's current weekly goal progess for exercise time.
   static double userProgressExerciseTimeWeekly = 0;
+
+  /// Stores a user's current daily end goal for exercise time.
   static double userExerciseTimeEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for exercise time.
   static double userExerciseTimeWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set an exercise time daily goal.
   static bool isExerciseTimeGoalSet = false;
+
+  /// Indicates if the user has set an exercise time weekly goal.
   static bool isExerciseTimeWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily exercise time goal.
   static bool isExerciseTimeGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly exercise time goal.
   static bool isExerciseTimeWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for calories burned.
   static double userProgressCalGoal = 0;
+
+  /// Stores a user's current weekly goal progess for calories burned.
   static double userProgressCalGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for calories burned.
   static double userCalEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for calories burned.
   static double userCalWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a calories daily goal.
   static bool isCalGoalSet = false;
+
+  /// Indicates if the user has set a calories weekly goal.
   static bool isCalWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily calories goal.
   static bool isCalGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly calories goal.
   static bool isCalWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for steps taken.
   static double userProgressStepGoal = 0;
+
+  /// Stores a user's current weekly goal progess for steps taken.
   static double userProgressStepGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for steps taken.
   static double userStepEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for steps taken.
   static double userStepWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a steps daily goal.
   static bool isStepGoalSet = false;
+
+  /// Indicates if the user has set a steps weekly goal.
   static bool isStepWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily steps goal.
   static bool isStepGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly steps goal.
   static bool isStepWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for miles traveled.
   static double userProgressMileGoal = 0;
+
+  /// Stores a user's current weekly goal progess for miles traveled.
   static double userProgressMileGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for miles traveled.
   static double userMileEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for miles traveled.
   static double userMileWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a miles daily goal.
   static bool isMileGoalSet = false;
+
+  /// Indicates if the user has set a miles weekly goal.
   static bool isMileWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily miles goal.
   static bool isMileGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly miles goal.
   static bool isMileWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for minutes spent cycling.
   static double userProgressCyclingGoal = 0;
+
+  /// Stores a user's current weekly goal progess for minutes spent cycling.
   static double userProgressCyclingGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for minutes spent cycling.
   static double userCyclingEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for minutes spent cycling.
   static double userCyclingWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a cycling daily goal.
   static bool isCyclingGoalSet = false;
+
+  /// Indicates if the user has set a cycling weekly goal.
   static bool isCyclingWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily cycling goal.
   static bool isCyclingGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly cycling goal.
   static bool isCyclingWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for minutes spent rowing.
   static double userProgressRowingGoal = 0;
+
+  /// Stores a user's current weekly goal progess for minutes spent rowing.
   static double userProgressRowingGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for minutes spent rowing.
   static double userRowingEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for minutes spent rowing.
   static double userRowingWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a rowing daily goal.
   static bool isRowingGoalSet = false;
+
+  /// Indicates if the user has set a rowing weekly goal.
   static bool isRowingWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily rowing goal.
   static bool isRowingGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly rowing goal.
   static bool isRowingWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for minutes spent using a step mill.
   static double userProgressStepMillGoal = 0;
+
+  /// Stores a user's current weekly goal progess for minutes spent using a step mill.
   static double userProgressStepMillGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for minutes spent using a step mill.
   static double userStepMillEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for minutes spent using a step mill.
   static double userStepMillWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a step mill daily goal.
   static bool isStepMillGoalSet = false;
+
+  /// Indicates if the user has set a step mill weekly goal.
   static bool isStepMillWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily step mill goal.
   static bool isStepMillGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly step mill goal.
   static bool isStepMillWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for minutes spent using an elliptical.
   static double userProgressEllipticalGoal = 0;
+
+  /// Stores a user's current weekly goal progess for minutes spent using an elliptical.
   static double userProgressEllipticalGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for minutes spent using an elliptical.
   static double userEllipticalEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for minutes spent using an elliptical.
   static double userEllipticalWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a elliptical daily goal.
   static bool isEllipticalGoalSet = false;
+
+  /// Indicates if the user has set a elliptical weekly goal.
   static bool isEllipticalWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily elliptical goal.
   static bool isEllipticalGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly elliptical goal.
   static bool isEllipticalWeeklyGoalComplete = false;
 
+  /// Stores a user's current daily goal progess for minutes spent resistance/strength training.
   static double userProgressResistanceStrengthGoal = 0;
+
+  /// Stores a user's current weekly goal progess for minutes spent resistance/strength training.
   static double userProgressResistanceStrengthGoalWeekly = 0;
+
+  /// Stores a user's current daily end goal for minutes spent resistance/strength training.
   static double userResistanceStrengthEndGoal = 0;
+
+  /// Stores a user's current weekly end goal for minutes spent resistance/strength training.
   static double userResistanceStrengthWeeklyEndGoal = 0;
+
+  /// Indicates if the user has set a resistance/strength daily goal.
   static bool isResistanceStrengthGoalSet = false;
+
+  /// Indicates if the user has set a resistance/strength weekly goal.
   static bool isResistanceStrengthWeeklyGoalSet = false;
+
+  /// Indicates if the user has completed their daily resistance/strength goal.
   static bool isResistanceStrengthGoalComplete = false;
+
+  /// Indicates if the user has completed their weekly resistance/strength goal.
   static bool isResistanceStrengthWeeklyGoalComplete = false;
 
+  /// Converts an activity duration string to a [Duration] object.
+  ///
+  /// Example:
+  ///
+  /// String activityDurationStr = "4 minutes";
+  ///
+  /// Duration d = convertToDuration(activityDurationStr);
+  ///
+  /// print(d); // 00:04:00
   static Duration convertToDuration(String activityDurationStr) {
     Duration duration = Duration.zero;
     String value = activityDurationStr.split(' ')[0];
@@ -121,6 +347,7 @@ class Goal {
     return duration;
   }
 
+  /// Calculates a user's completed goals.
   static void _calculateCompletedGoals() {
     isExerciseTimeGoalComplete = isExerciseTimeGoalSet &&
         (userProgressExerciseTime / userExerciseTimeEndGoal) * 100 >= 100;
@@ -188,6 +415,11 @@ class Goal {
                 100;
   }
 
+  /// Uploads a completed daily goal's stats to the user's goal log collection.
+  /// 
+  /// Once a goal is completed, the associated goal values are reset.
+  /// 
+  /// A user will also receive a local notification informing them of completion.
   static void _uploadCompletedDailyGoals() {
     if (isExerciseTimeGoalComplete) {
       FireStore.getGoalLogCollection(goalType: "daily").add({
@@ -323,6 +555,11 @@ class Goal {
     }
   }
 
+  /// Uploads a completed weekly goal's stats to the user's goal log collection.
+  /// 
+  /// Once a goal is completed, the associated goal values are reset.
+  /// 
+  /// A user will also receive a local notification informing them of completion.
   static void _uploadCompletedWeeklyGoals() {
     if (isExerciseTimeWeeklyGoalComplete) {
       FireStore.getGoalLogCollection(goalType: "weekly").add({
@@ -478,6 +715,9 @@ class Goal {
     }
   }
 
+  /// Checks if a user's weekly goal deadline has been reached.
+  /// 
+  /// If so, the associated goal's values will be reset.
   static void _checkWeeklyGoalDeadLines() {
     if (exerciseWeekDeadline == "${_now.month}/${_now.day}/${_now.year}") {
       FireStore.updateGoalData({
@@ -554,12 +794,16 @@ class Goal {
     }
   }
 
+  /// Checks if [dayOfMonth] is equal to the current day.
+  /// 
+  /// If not, the associated field in Firestore is updated.
   static void _updateDayOfMonth() {
     if (dayOfMonth != _now.day) {
       FireStore.updateGoalData({"dayOfMonth": _now.day});
     }
   }
 
+  /// Upload's a user's completed goals to their goal-log Firestore collection.
   static void uploadCompletedGoals() {
     _calculateCompletedGoals();
     _uploadCompletedDailyGoals();
