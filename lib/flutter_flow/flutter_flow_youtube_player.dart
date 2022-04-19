39649e0938e10fc0fb8f0978eaf0b455e81d0eae
Brian Walker Jr <thaballa79@gmail.com>
2022-04-19 15:48:00 -0400

    migrates from discontinued package

--- lib/flutter_flow/flutter_flow_youtube_player.dart
+++ lib/flutter_flow/flutter_flow_youtube_player.dart
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Sarbagya Dhaubanjar. All rights reserved.
+Copyright 2021 jsml. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
@@ -24,7 +24,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 import 'package:flutter/material.dart';
-import 'package:youtube_plyr_iframe/youtube_plyr_iframe.dart';
+import 'package:modjs_video_player/modjs_video_player.dart';
 
 const kYoutubeAspectRatio = 16 / 9;
 
